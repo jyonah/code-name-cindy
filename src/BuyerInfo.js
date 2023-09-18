cdf7233b422dfcece5bdd0a1d79b1a0cd7de24b0
Jonah Price <jyoonah@gmail.com>
2023-09-18 15:19:30 -0700

    Updates Buyer Info to interact with context and state.

--- src/BuyerInfo.js
+++ src/BuyerInfo.js
@@ -1,4 +1,21 @@
+import {useState} from "react";
+import {useBandFormContext} from "./BandFormContext";
+
 export function BuyerInfo() {
+    const {formData, setFormData} = useBandFormContext()
+
+    function handleChange(e) {
+        const {name, value} = e.target.valueOf()
+
+        setFormData(prevState => ({
+            ...prevState,
+            buyer: {
+                ...prevState.buyer,
+                [name]: value
+            }
+        }))
+    }
+
     return (
         <div className='form-section'>
             <div>
@@ -7,6 +24,8 @@ export function BuyerInfo() {
                     id='firstName'
                     name='firstName'
                     placeholder='First Name'
+                    value={formData.buyer?.firstName ?? ''}
+                    onChange={handleChange}
                     required
                 />
                 <input
@@ -14,6 +33,8 @@ export function BuyerInfo() {
                     id='lastName'
                     name='lastName'
                     placeholder='Last Name'
+                    value={formData.buyer?.lastName ?? ""}
+                    onChange={handleChange}
                     required
                 />
             </div>
@@ -23,6 +44,8 @@ export function BuyerInfo() {
                     id='address'
                     name='address'
                     placeholder='Address'
+                    value={formData.buyer?.address ?? ""}
+                    onChange={handleChange}
                     required
                 />
             </div>
